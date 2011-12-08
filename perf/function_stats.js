187551cd885b84e06edfdface7762b3d5dfca5d4
Erick Johnson <erick@junctionnetworks.com>
2011-12-08 15:43:01 -0500

    perf/function_stats: added a naive mechanism to help audit the numbers of function calls made

---
+++ perf/function_stats.js
@@ -0,0 +1,40 @@
+DataStructures.TRACK_STATS = NO;
+
+DataStructures.FunctionStats = {
+  _callStats: {},
+
+  startFunction: function(name) {
+    if (!DataStructures.TRACK_STATS) return this;
+
+    this._callStats[name] = this._callStats[name] || {};
+    this._callStats[name].count = this._callStats[name].count || 0;
+    this._callStats[name].count++;
+    return this;
+  },
+
+  dump: function() {
+    if (!DataStructures.TRACK_STATS) {
+      SC.Logger.warn('DataStructures.FunctionStats.dump: DS.TRACK_STATS is not enabled');
+      return this;
+    }
+
+    SC.Logger.group('FunctionStats Dump');
+    var countTotal = 0;
+    for(var p in this._callStats) if (this._callStats.hasOwnProperty(p)) {
+      var count = this._callStats[p].count;
+      SC.Logger.group(p);
+      SC.Logger.log("Call Count: %@".fmt(count));
+      countTotal += count;
+      SC.Logger.groupEnd();
+    }
+    SC.Logger.log("Total Count:",countTotal);
+    SC.Logger.groupEnd();
+    return this;
+  },
+
+  clear: function() {
+    this._callStats = {};
+    return this;
+  }
+};
+
